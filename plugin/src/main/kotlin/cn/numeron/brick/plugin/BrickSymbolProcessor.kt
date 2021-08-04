60e3d35cd4af1d1b04ceffaf4f57e5b50e637dc6
Tears丶残阳 <x.z.y.seido@outlook.com>
2021-08-04 18:04:30 +0800

    change to generate from all files.

--- plugin/src/main/kotlin/cn/numeron/brick/plugin/BrickSymbolProcessor.kt
+++ plugin/src/main/kotlin/cn/numeron/brick/plugin/BrickSymbolProcessor.kt
@@ -5,12 +5,12 @@ import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.symbol.KSAnnotated
 
-class BrickSymbolProcessor(private val environment: SymbolProcessorEnvironment) : SymbolProcessor {
+class BrickSymbolProcessor(environment: SymbolProcessorEnvironment) : SymbolProcessor {
 
     private val instanceOfTypeVisitor = ViewModelsVisitor(environment)
 
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        resolver.getNewFiles().forEach {
+        resolver.getAllFiles().forEach {
             it.accept(instanceOfTypeVisitor, Unit)
         }
         return emptyList()
